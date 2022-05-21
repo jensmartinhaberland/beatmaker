d29912cc5f2229d865c424d7288545eadff324b4
Jens-Martin Haberland <jensmartin.haberland@icloud.com>
2022-05-21 11:37:51 +0200

    add grid to screen

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Build Your Own Beat Maker
 import pygame
 
 pygame.init()
@@ -16,16 +15,47 @@ label_font = pygame.font.Font('Roboto-Bold.ttf', 32)
 
 fps = 60
 timer = pygame.time.Clock()
+beats = 8
+instruments = 6
+
+
+def draw_grid():
+    left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT - 200], 5)
+    bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT - 200, WIDTH, 200], 5)
+    boxes = []
+    colors = [gray, white, gray]
+    hi_hat_text = label_font.render('Hi Hat', True, white)
+    screen.blit(hi_hat_text, (30, 30))
+    snare_text = label_font.render('Snare', True, white)
+    screen.blit(snare_text, (30, 130))
+    kick_text = label_font.render('Kick', True, white)
+    screen.blit(kick_text, (30, 230))
+    crash_text = label_font.render('Crash', True, white)
+    screen.blit(crash_text, (30, 330))
+    clap_text = label_font.render('Clap', True, white)
+    screen.blit(clap_text, (30, 430))
+    tom_text = label_font.render('Tom', True, white)
+    screen.blit(tom_text, (30, 530))
+    for i in range(instruments):
+        pygame.draw.line(screen, gray, (0, (i * 100) + 100), (200, (i * 100) + 100), 3)
+
+    for i in range(beats):
+        for j in range(instruments):
+            rect = pygame.draw.rect(screen, gray,
+                                    [i * ((WIDTH - 200) // beats) + 200, (j * 100), ((WIDTH - 200) // beats),
+                                     ((HEIGHT - 200) // instruments)], 5, 5)
+
 
 run = True
+
 while run:
     timer.tick(fps)
     screen.fill(black)
+    draw_grid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.flip()
-
 pygame.quit()
